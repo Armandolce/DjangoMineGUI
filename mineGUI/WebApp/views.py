d13896cdd587719077c9c1d6ef458a8d47a2da3a
Armando Ponce Soriano <aponcesoriano@hotmail.com>
2022-11-28 17:50:04 -0600

    Empezando otro proyecto, otra vez XDD

--- mineGUI/WebApp/views.py
+++ mineGUI/WebApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def pyscript(request):
+    return render(request, 'pyscript.html')
+
+def EDA(request):
+    return render(request, 'EDA.html')
